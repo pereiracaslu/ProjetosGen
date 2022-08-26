045faf050cf896c4afa8fa9263a28bb23a4b09d8
pereiracaslu <lucascosta2512@gmail.com>
2022-08-25 22:51:47 -0300

    Exercícios de banco relacional

---
+++ Bloco_2/mysql_relacional_exercicios/Atividade 2.sql
@@ -0,0 +1,85 @@
+/*Atividade 2 - 
+*/
+
+CREATE DATABASE db_pizzaria_legal;
+
+USE db_pizzaria_legal;
+
+CREATE TABLE tb_categorias(
+	 /* chave primaria sempre deverá existir nas tabelas com bancos de dados relacionais*/
+     id bigint auto_increment primary key,
+     descricao varchar (100),
+     tamanho varchar (100),
+     borda_recheada boolean
+);
+
+CREATE TABLE tb_pizzas(
+	id bigint auto_increment primary key,
+    sabor varchar (100),
+    preco decimal (6,2),
+    massa varchar (100),
+    quantidade int,
+    categoria_id bigint,
+    
+    FOREIGN KEY(categoria_id) REFERENCES tb_categorias(id)
+);
+
+	SELECT *FROM tb_categorias;
+    
+    INSERT INTO tb_categorias (descricao, tamanho, borda_recheada) VALUES ("Salgada","Brotinho", true);
+    INSERT INTO tb_categorias (descricao, tamanho, borda_recheada) VALUES ("Salgada","Brotinho", false);
+    INSERT INTO tb_categorias (descricao, tamanho, borda_recheada) VALUES ("Salgada","Média", false);
+    INSERT INTO tb_categorias (descricao, tamanho, borda_recheada) VALUES ("Salgada","Media", true);
+    INSERT INTO tb_categorias (descricao, tamanho, borda_recheada) VALUES ("Salgada","Grande", false);
+    INSERT INTO tb_categorias (descricao, tamanho, borda_recheada) VALUES ("Salgada","Grande", true);
+    
+    INSERT INTO tb_categorias (descricao, tamanho, borda_recheada) VALUES ("Doce","Brotinho", true);
+    INSERT INTO tb_categorias (descricao, tamanho, borda_recheada) VALUES ("Doce","Grande", true);
+    
+    INSERT INTO tb_categorias (descricao, tamanho, borda_recheada) VALUES ("Vegetariana","Brotinho", true);
+    INSERT INTO tb_categorias (descricao, tamanho, borda_recheada) VALUES ("Vegana","Brotinho", false);
+    
+    SELECT * FROM tb_pizzas;
+    
+    INSERT INTO tb_pizzas(sabor, preco, massa, quantidade, categoria_id)
+    VALUES ("Frango C/ Catupiry", 30.00, "Tradicional", 5, 1);
+    
+    INSERT INTO tb_pizzas(sabor, preco, massa, quantidade, categoria_id)
+    VALUES ("Muçarela", 40.99, "Tradicional", 5, 2);
+    
+    INSERT INTO tb_pizzas(sabor, preco, massa, quantidade, categoria_id)
+    VALUES ("Prestígio", 50.99, "Tradicional", 2, 8);
+    
+    INSERT INTO tb_pizzas(sabor, preco, massa, quantidade, categoria_id)
+    VALUES ("Lombo C/ Catupiry", 60.99, "Tradicional", 3, 6);
+ 
+    INSERT INTO tb_pizzas(sabor, preco, massa, quantidade, categoria_id)
+    VALUES ("Brócolis C/ Catupiry", 40.99, "Tradicional", 2, 9);
+    
+    INSERT INTO tb_pizzas(sabor, preco, massa, quantidade, categoria_id)
+    VALUES ("Calabresa", 45.99, "Tradicional", 5, 1);
+    
+    SELECT * FROM tb_pizzas WHERE preco > 45.00 ORDER BY preco DESC; /*Decrescente*/
+    SELECT * FROM tb_pizzas WHERE preco < 45.00 ORDER BY preco ASC; /*Crescente*/
+    
+    SELECT * FROM tb_pizzas WHERE preco >= 50 AND preco <= 100;
+    SELECT * FROM tb_pizzas WHERE preco BETWEEN 50 and 100;
+    
+    /*Like é usado para trazer as palavras que estão na coluna que incializa, tenha ou finaliza
+    exemplo = like "%c%" = tenha; like "c%" = comece; like "%c" = termine */
+    SELECT * FROM tb_pizzas WHERE sabor LIKE  "%M%";
+		
+        
+	SELECT * FROM tb_pizzas INNER JOIN tb_categorias
+    ON tb_categorias.id = tb_pizzas.categoria_id;
+	
+    SELECT * FROM tb_pizzas INNER JOIN tb_categorias
+    ON tb_categorias.id = tb_pizzas.categoria_id;
+    
+    SELECT * FROM tb_pizzas INNER JOIN tb_categorias
+    ON tb_categorias.descricao = "Salgada";
+	
+    
+    
+    
+    
---
+++ Bloco_2/mysql_relacional_exercicios/Atividade3.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE db_cidade_das_carnes;
+USE db_cidade_das_carnes;
+
+CREATE TABLE tb_categorias(
+	id int primary key auto_increment,
+    tipoAnimal varchar(60),
+    descricao varchar(60)
+);
+
+CREATE TABLE tb_produtos(
+	id int primary key auto_increment,
+    tipo varchar(50),
+    peso decimal (7,3),
+    preco decimal(6,2),
+    categoria_id int,
+    FOREIGN KEY(categoria_id) REFERENCES tb_categorias(id)
+);
+
+INSERT INTO tb_categorias(tipoAnimal, descricao)
+values 
+("Bovina", "carne saborosa"),
+("Suina", "carne saborosa"),
+("Aviaria", "Frango saboroso"),
+("Frutos do Mar", "pexe saboroso"),
+("Caprino", "Carne saborosa");
+
+
+SELECT * FROM tb_produtos;
+INSERT INTO tb_produtos(tipo, peso,preco,categoria_id)
+values
+("Picanha", 80.300, 58.00, 1),
+("Peito de Frango", 100.730, 25.00, 3),
+("Contra File", 50.480, 40.00, 1),
+("Costela Suina", 400.620, 58.00, 2),
+("Paleta", 100.620, 48.00, 5),
+("Salmao", 120.620, 58.00, 4);
+
+SELECT * FROM tb_produtos WHERE preco > 50.00;
+
+SELECT * FROM tb_produtos WHERE preco BETWEEN 50 AND 150;
+
+SELECT * FROM tb_produtos WHERE tipo LIKE "%c%";
+
+SELECT * FROM tb_produtos INNER JOIN tb_categorias
+ON tb_categorias.id = tb_produtos.categoria_id;
+
+SELECT * FROM tb_produtos INNER JOIN tb_categorias WHERE tb_categorias.id = 2 AND tb_produtos.categoria_id = 2;
